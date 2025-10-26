f912a39c6a29e9f6e2fbf09f1a8ff862e71ea8d8
Simpf56 <xsorex561@gmail.com>
2025-10-26 18:17:30 +0100

    Update LV3_2.c

--- KonzolnaAplikacija/KonzolnaAplikacija/LV3_2.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV3_2.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 int LV3_2(void) {
-	char znakJedan, znakDva;
+	int znakJedan, znakDva;
 
 	printf("Unesite dva znaka:\n");
-	scanf("%c", &znakJedan);
+	scanf("%d", &znakJedan);
 
 	printf("Unesite drugi znak:\n");
-	scanf(" %c", &znakDva);
+	scanf(" %d", &znakDva);
 
 	printf("Prvi znak je %o oktalno i %x heksadecimalno.\n",znakJedan,znakJedan);
 	printf("Drugi znak je %o oktalno a %x heksadecimalo.\n", znakDva, znakDva);
