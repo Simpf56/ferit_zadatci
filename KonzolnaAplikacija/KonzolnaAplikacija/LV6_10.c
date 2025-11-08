96eb156ea3dc73b0448128e7eb11c8b8b8da15d5
Simpf56 <xsorex561@gmail.com>
2025-11-08 20:30:40 +0100

    LV6

--- KonzolnaAplikacija/KonzolnaAplikacija/LV6_10.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV6_10.c
@@ -2,7 +2,20 @@
 #include <stdio.h>
 
 int LV6_10() {
+	int n;
+	int broj;
+	int obrnuto = 0;
+	printf("Unesite broj: ");
+	scanf("%d", &n);
 
+	while (n > 0) {
+		broj = n % 10;
+		obrnuto = obrnuto * 10 + broj;
+		n = n / 10;
+
+	}
+
+	printf("Obrnuti broj je %d.", obrnuto);
 
 	return 0;
 }
--- KonzolnaAplikacija/KonzolnaAplikacija/LV6_9.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV6_9.c
@@ -2,7 +2,17 @@
 #include <stdio.h>
 
 int LV6_9() {
+	float suma = 0, sredina;
+	int i,brojac = 0;
 
+	for (i = 100; i < 1000; ++i) {
+		if (i % 3 == 0) {
+			brojac++;
+			suma += i;
+		}
+	}
+
+	printf("Aritmeticka sredina je %.2f",suma/brojac);
 
 	return 0;
 }
