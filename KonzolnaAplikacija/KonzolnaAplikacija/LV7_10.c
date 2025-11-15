42df183995be50d4bd054a9b75ab7cb444048ab5
Simpf56 <xsorex561@gmail.com>
2025-11-15 22:16:22 +0100

    LV7

--- KonzolnaAplikacija/KonzolnaAplikacija/LV7_10.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV7_10.c
@@ -1,8 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define VEL_NIZA 24
 
 int LV7_10() {
+	int n, i;
+	int niz[VEL_NIZA];
+
+	printf("Unesite broj elemenata niza ( 6 - 24): \n");
+	scanf("%d", &n);
+	if (n < 6 || n >= 25) {
+		printf("Krivi unos!");
+		return 2;
+	}
+
+	printf("Upisite %d cijelih brojeva: \n", n);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &niz[i]);
+	}
+	
+
+	for (i = 0; i < n/2; i++) {
+		int rezerva = niz[i];
+		niz[i] = niz[n - 1 - i];
+		niz[n - 1 - i] = rezerva;
+	}
+
+	printf("Vas niz je: \n");
+	for (i = 0; i < n; i++) {
+		printf("%d ",niz[i]);
+	}
 
 
 	return 0;
 }
+
+/*Omogućiti korisniku unos n (6 ≤ n < 25) vrijednosti elemenata u polje cijelih brojeva. Zamijeniti
+mjesta prvoj polovici polja s drugom polovicom polja. Npr. zamijeniti mjesta prvom elementu polja
+sa zadnjim elementom, drugom elementu polja s predzadnjim elementom i tako skroz do polovice
+polja.*/
--- KonzolnaAplikacija/KonzolnaAplikacija/LV7_9.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV7_9.c
@@ -1,8 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define VEL_NIZA 12
 
 int LV7_9() {
+	int q, i;
+	float niz[VEL_NIZA];
 
+	printf("Unesite broj elemenata niza (6-12): \n");
+	scanf("%d", &q);
+	if (q <= 5 || q > 12) {
+		printf("Krivi unos!");
+		return 2;
+	}
 
+	for (i = 0; i < q; i++) {
+		scanf("%f", &niz[i]);
+		if ((int)niz[i] % 2 == 0) {
+			niz[i] /= 10;
+		}
+		else {
+			niz[i] /= (-10);
+		}
+	}
+
+	printf("Brojevi nizua su: \n");
+	for (i = 0; i < q; i++) {
+		printf("%.2f ",niz[i]);
+	}
+		
 	return 0;
 }
+
+/*Omogućiti korisniku unos q (5 < q ≤ 12) vrijednosti elemenata u polje realnih brojeva. Podijeliti
+vrijednosti svakog elementa polja koji ima parni indeks s vrijednosti 10, a koji ima neparni indeks s
+vrijednosti -10. Potom na ekran ispisati izmijenjeno polje.*/
