2cb9fdb95f9f5274f072332c8d7df8a5d9bf016d
Simpf56 <xsorex561@gmail.com>
2025-11-22 21:40:44 +0100

    LV8

--- KonzolnaAplikacija/KonzolnaAplikacija/LV8_2.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV8_2.c
@@ -1,8 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+#define NIZ 51
 
 int LV8_2() {
+	int i, brojac = 0;
+	char niz[NIZ];
+	char znak;
+	int duzina;
+
+	printf("Unesite 50 znakova: \n");
+	scanf("%50[^\n]", niz);
+
+	duzina = strlen(niz);
+
+	printf("Unesite znak koji tražite: \n");
+	scanf(" %c", &znak);
+
+	for (i = 0; i < duzina; i++) {
+		if (niz[i]==znak) {
+			brojac++;
+		}
+	}	
+
+	printf("Znak %c se pojavljuje: %d puta.\n",znak,brojac);
+
+	printf("Znak se nalazi na mjestima: \n");
+	for (i = 0; i < duzina; i++) {
+		if (niz[i] == znak) {
+			printf("%d ", i + 1);
+		}
+	}
+
 
 
 	return 0;
 }
+
+
+//Tražiti od korisnika unos jednog stringa od maksimalno 50 znakova.Provjeriti i na ekran ispisati
+//koliko se puta pojavljuje neka znamenka te na kojim mjestima.
--- KonzolnaAplikacija/KonzolnaAplikacija/LV8_9.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV8_9.c
@@ -1,8 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define NIZ 111
 
 int LV8_9() {
+	int i, j,z;
+	char niz[NIZ];
+	char nizdva[NIZ];
+	int duzina = 0;
+	char c = ' ';
+
+	printf("Unesite vas string sa maximalno 110 znakova: \n");
+	scanf("%110[^\n]", niz);
+	duzina = strlen(niz);
+
+	for (i = 0; i < duzina; i++) {
+		
+
+		if(niz[i] >= 65 && niz[i] <=90)
+			for (j = 65; j < 91; j++) {
+				if (j == niz[i]) {
+					nizdva[i] = niz[i]+32;
+				}
+			}
+		else if (niz[i] >= 97 && niz[i] <= 122) {
+			for (z = 97; z < 123; z++) {
+				if (z == niz[i]) {
+					nizdva[i] = niz[i] - 32;
+				}
+			}
+		}
+		else {
+			nizdva[i] = niz[i];
+		}
+	}
+
+	printf("REZULTATI:\n");
+	for (i = 0; i < duzina; i++) {
+		printf("%c", nizdva[i]);
+	}
 
 
 	return 0;
 }
+
+/*Omogućiti korisniku unos jednog stringa od maksimalno 110 znakova. Potom zamijeniti svako malo
+slovo ekvivalentnim velikim slovom i obrnuto. Na kraju ispisati string na ekran. Nije dozvoljeno
+koristiti funkcije standardne biblioteke za ostvarivanje zamjene slova.*/
