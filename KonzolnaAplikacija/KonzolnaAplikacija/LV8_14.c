4c5ef9f6a5f6483fb409538a4e591bc819fb9a3e
Simpf56 <xsorex561@gmail.com>
2025-11-22 23:18:17 +0100

    LV8

--- KonzolnaAplikacija/KonzolnaAplikacija/LV8_14.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV8_14.c
@@ -1,8 +1,58 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+#define NIZ 181
 
 int LV8_14() {
+    int i,duzina,pozicijaDvotocke,dvotocka = 0;
+    char c;
+    char prije[NIZ];
+    char poslije[NIZ];
 
+    printf("Unesite vas string (maximalno 180 znakova): \n");
+    scanf("%180[^\n]", prije);
+    duzina = strlen(prije);
+
+    for (i = 0; i < duzina; i++) {
+        if (prije[i] == ':') {
+            dvotocka = 1;
+            pozicijaDvotocke = i;
+            break;
+        }
+    }
+
+    if (dvotocka == 0) {
+        printf("REZULTATI:\n");
+        printf("Nema dvotocke");
+        return 0;
+    }
+
+    printf("REZULTATI:\n");
+    for (i = pozicijaDvotocke + 1; i < duzina; i++) {
+        printf("%c", prije[i]);
+    }
+
+    
 
 	return 0;
 }
+
+//for (i = 0; i < duzina; i++) {
+//    if (prije[i] == ':') {
+//        dvotocka = 1;
+//        break;
+//    }
+//}
+//
+
+//}
+//
+//scanf("%180[^\n]", poslije);
+//printf("REZULTAT:\n");
+//printf("Poslije: %s\n", poslije);
+
+
+//Napisati program u C jeziku koji omogućava korisniku unos jednog stringa od maksimalno 180 znakova(VPL se ponaša kao unos s tipkovnice).
+//Provjeriti nalazili se negdje u stringu znak dvotočke(:), 
+//te ako se on pronađe potrebno je sve iza tog znaka zapisati u novi string te nakon poruke REZULTATI : \n ispisati na ekran izmijenjeni string.
+//Testiranje obaviti unutar main() funkcije.Ukoliko u stringu nema dvotočke ispisati samo prikladnu poruku na ekran : "Nema dvotocke".
