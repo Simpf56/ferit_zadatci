b5daf1baa892f54b6507ebb29023000a2a40520a
Simpf56 <xsorex561@gmail.com>
2025-11-29 01:07:55 +0100

    LV9

--- KonzolnaAplikacija/KonzolnaAplikacija/LV9_10.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV9_10.c
@@ -1,8 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+int trokut(int a, int b, int c);
 
 int LV9_10() {
-
-
+	int prva, druga, treca;
+	int stranica;
+	printf("Unesite stranice vašeg trokuta:\n");
+	scanf("%d %d %d", &prva, &druga, &treca);
+	stranica = trokut(prva, druga, treca);
+	printf("REZULTAT:\n");
+	switch (stranica) {
+		case 0: printf("Stranice ne cine trokut!");
+			break;
+		case 1: printf("Jednakracan trokut");
+			break;
+		case 2: printf("Jednakostranican trokut");
+			break;
+		case 3: printf("Raznostranican trokut");
+			break;
+		default: printf("Krivi unos!");
+			break;
+	}
 	return 0;
 }
+
+int trokut(int a, int b, int c) {
+	if (b + c < a && c + a < b && a + b < c) {
+		return 0;
+	}
+	
+	if (a == b && b == c) {
+		return 2;
+	}
+	else if (a == b || a == c || b == c ) {
+		return 1;
+	}
+	else {
+		return 3;
+	}
+}
+
+/*Omogućiti korisniku unos duljine tri stranice trokuta a, b i c. Zatim te tri stranice poslati u funkciju
+koja vraća 0 ako stranice ne čine trokut, 1 ako je trokut jednakostraničan, 2 ako je trokut
+jednakokračan i 3 ako je trokut raznostraničan. U glavnom dijelu programa ispisati odgovarajuću
+poruku, ako stranice ne tvore trokut, a ako stranice čine trokut, ispisati kakav trokuta čine stranice.*/
--- KonzolnaAplikacija/KonzolnaAplikacija/LV9_9.c
+++ KonzolnaAplikacija/KonzolnaAplikacija/LV9_9.c
@@ -2,7 +2,26 @@
 #include <stdio.h>
 
 int LV9_9() {
+	int cijeliBroj;
+	int obrnuti;
+	printf("Unesite vas broj:\n");
+	scanf("%d", &cijeliBroj);
 
-
+	obrnuti = redoslijed(cijeliBroj);
+	printf("REZULTAT:\n");
+	printf("%d", obrnuti);
 	return 0;
 }
+
+int redoslijed(int broj) {
+	int novi = 0;
+	while (broj > 0) {
+		novi = novi * 10 + broj % 10;
+		broj /= 10;
+	}
+	return novi;
+}
+
+/*Omogućiti korisniku unos jednog cijelog broja. Zatim napisati funkciju kojoj se predaje taj broj i iz
+predanog cijelog broja izračunava i vraća broj u obrnutom redoslijedu, npr. Predaje se broj 1234,
+a funkcija vraća 4321. Ispisati izmijenjeni broj u glavnom dijelu programa.*/
